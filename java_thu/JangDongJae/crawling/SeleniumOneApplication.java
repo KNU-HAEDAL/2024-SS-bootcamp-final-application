c0c194c0508687ec465694ebabeead6a34a4e401
JangDJ <163510837+JangDJ1227@users.noreply.github.com>
2024-06-08 22:37:49 +0900

    장동재2

---
+++ java_thu/JangDongJae/crawling/SeleniumOneApplication.java
@@ -0,0 +1,13 @@
+package haedal.selenium_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumOneApplication.class, args);
+	}
+
+}
